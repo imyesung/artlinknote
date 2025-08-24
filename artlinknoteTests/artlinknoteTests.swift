8fb724566aea98c6907fbe83f8c795c18c11f11b
imyesung <limyesung@gmail.com>
2025-08-24 09:50:37 +0900

    Initial Commit

---
+++ artlinknoteTests/artlinknoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  artlinknoteTests.swift
+//  artlinknoteTests
+//
+//  Created by yesunglim on 2025-08-24.
+//
+
+import Testing
+@testable import artlinknote
+
+struct artlinknoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
